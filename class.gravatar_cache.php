32f7a8e5c467ee07765edfc260d9fd8785d7927f
Almaz <diamond.scalping@yandex.com>
2016-10-17 17:34:14 +0500

    v1.0.3.4

--- class.gravatar_cache.php
+++ class.gravatar_cache.php
@@ -1,21 +1,4 @@
 <?php
-/*
-  Copyright (C) 2016 DevDiamond. (email : me@devdiamond.com)
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
 /**
  * Class WP_GravatarCache
  *
@@ -30,9 +13,9 @@
  *      );
  *
  * @link    https://github.com/DevDiamondCom/WP_GravatarCache
- * @version 1.0.3.3
+ * @version 1.0.3.4
  * @author  DevDiamond <me@devdiamond.com>
- * @license GPLv2 or later
+ * @license GPLv3 or later - http://www.gnu.org/licenses/gpl-3.0.html
  */
 class WP_GravatarCache
 {
